259a0cc01fce9abf7c61ea6611d65ea1a69325a4
Souta Fujimoto <souta.fujimoto@wealth-park.com>
2022-01-11 05:45:38 +0900

    create year to secs

---
+++ PGFB/year_to_secs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int year = 1;
+
+    // days
+    cout << year * 365 << endl;
+    // hrs
+    cout << year * 365 * 24 << endl;
+    // mins
+    cout << year * 365 * 24 * 60 << endl;
+    //  secs
+    cout << year * 365 * 24 * 60 * 60 << endl;
+}
