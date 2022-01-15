a6414e1bc2e549d41d91df342af1a15d807ffaab
Souta Fujimoto <souta.fujimoto@wealth-park.com>
2022-01-16 03:09:09 +0900

    Create bar_graph.cpp

---
+++ PGFB/bar_graph.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    // ここにプログラムを追記
+    int i = 0;
+
+    cout << "A:";
+    while(i < A){
+        cout << "]";
+        i++;
+    }
+
+    cout << endl;
+    i = 0;
+
+    cout << "B:";
+
+    while (i < B)
+    {
+        cout << "]";
+        i++;
+    }
+
+    cout << endl;
+}
