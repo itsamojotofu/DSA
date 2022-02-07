c4f3527fe80bf5b7a8c467ed23c5351be2876216
Souta Fujimoto <souta.fujimoto@wealth-park.com>
2022-02-08 07:00:23 +0900

    c_and_p game_contest answer

--- PGFB/game_contest.cpp
+++ PGFB/game_contest.cpp
@@ -11,14 +11,31 @@ int main()
     cin >> A.at(i) >> B.at(i);
   }
 
-  vector<vector<int>> data(N, vector<int>(N));
+  // N×Nのchar型の2次元配列のすべての要素を'-'で初期化
+  vector<vector<char>> table(N, vector<char>(N, '-'));
 
-  // 入力 (2重ループを用いる)
+  for (int i = 0; i < M; i++)
+  {
+    // 1〜N → 0〜N-1 に変換
+    A.at(i)--;
+    B.at(i)--;
+    table.at(A.at(i)).at(B.at(i)) = 'o'; // AはBに勝った
+    table.at(B.at(i)).at(A.at(i)) = 'x'; // BはAに負けた
+  }
+
   for (int i = 0; i < N; i++)
   {
     for (int j = 0; j < N; j++)
     {
-      cin >> data.at(i).at(j);
+      cout << table.at(i).at(j);
+      if (j == N - 1)
+      {
+        cout << endl; // 行末なら改行
+      }
+      else
+      {
+        cout << " "; // 行末でないなら空白を出力
+      }
     }
   }
 }
